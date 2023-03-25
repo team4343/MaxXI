f02eff2a238bcf1cc79c047e17efd3950a03e96a
Bobbbbay <abatterysingle@gmail.com>
2023-03-25 11:56:19 -0400

    feat(DriveToPoint): implement initial trajectory from current pose generation

--- src/main/java/com/maxtech/maxxi/commands/drivebase/DriveToPoint.java
+++ src/main/java/com/maxtech/maxxi/commands/drivebase/DriveToPoint.java
@@ -1,19 +1,29 @@
 package com.maxtech.maxxi.commands.drivebase;
 
+import java.util.ArrayList;
 import com.maxtech.maxxi.subsystems.DrivetrainSubsystem;
+import edu.wpi.first.math.controller.HolonomicDriveController;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.maxtech.lib.swervelib.math.SwerveModuleState2;
 
 public class DriveToPoint extends CommandBase {
-    private final Trajectory trajectory;
-    
+    private final Pose2d pose;
+
     private final DrivetrainSubsystem drivetrainSubsystem;
 
-    public DriveToPoint(Trajectory trajectory, DrivetrainSubsystem drivetrainSubsystem) {
-        this.trajectory = trajectory;
+    private boolean finished = false;
+
+    public DriveToPoint(Pose2d pose, DrivetrainSubsystem drivetrainSubsystem) {
+        this.pose = pose;
 
         this.drivetrainSubsystem = drivetrainSubsystem;
 
@@ -22,12 +32,45 @@ public class DriveToPoint extends CommandBase {
 
     @Override
     public void execute() {
-        var states = trajectory.getStates();
+        var currentPose = drivetrainSubsystem.getPose();
+        var goalPose = pose.relativeTo(currentPose);
 
-        states.forEach(state -> {
-            var relativePose = state.poseMeters.relativeTo(drivetrainSubsystem.getPose());
+        // TODO: construct this better.
+        var waypoints = new ArrayList<Pose2d>();
+        waypoints.add(currentPose);
+        waypoints.add(goalPose);
 
-            // Here, we have a robot-relative pose in meters, we just have to drive the bot to that point.
-        });
+        var trajectory =
+                TrajectoryGenerator.generateTrajectory(waypoints, new TrajectoryConfig(1, .1));
+
+        var controller = new HolonomicDriveController(new PIDController(1, 0, 0),
+                new PIDController(1, 0, 0),
+                new ProfiledPIDController(1, 0, 0, new TrapezoidProfile.Constraints(6.28, 3.14)));
+
+        var totalTime = trajectory.getTotalTimeSeconds();
+
+        var startTime = Timer.getFPGATimestamp();
+
+        // TODO: double check that this for method is right.
+        for (double time = Timer.getFPGATimestamp(); (time - startTime) <= totalTime;) {
+            // TODO: we may have to round the FPGATimestamp because it's too precise.
+            var sample = trajectory.sample(Timer.getFPGATimestamp());
+
+            // TODO: what's the rotation argument?
+            ChassisSpeeds adjustedSpeeds = controller.calculate(drivetrainSubsystem.getPose(),
+                    sample, sample.poseMeters.getRotation());
+
+            SwerveModuleState2[] moduleStates =
+                    drivetrainSubsystem.getKinematics().toSwerveModuleStates(adjustedSpeeds);
+
+            drivetrainSubsystem.swerveDrive.setModuleStates(moduleStates, false);
+        }
+
+        finished = true;
+    }
+
+    @Override
+    public boolean isFinished() {
+        return finished;
     }
 }
